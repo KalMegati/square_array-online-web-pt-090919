0785b2a00e9dfb08551c81f1dfd282349fe82ee6
Srikalyan Megati <kalmegati@gmail.com>
2019-09-20 04:23:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,14 @@
-def square_array(array)
-  new_array = []
-  array.each { |entry| new_array << entry**2 }
-  new_array
-end
+#def square_array(array)
+#  new_array = []
+#  array.each { |entry| new_array << entry**2 }
+#  new_array
+#end
 
+def square_array(array)
+ new_array = []
+ array.each do |new_numbers|
+   new_numbers **= 2
+   new_array.push(new_numbers)
+end
+ new_array
+end
